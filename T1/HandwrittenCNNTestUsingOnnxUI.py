7d6e7f2e7afd6bfed0243c8c1dba936d54faf36f
smithfox <ssm.fox@gmail.com>
2024-05-17 18:14:09 +0800

    T1: 主界面增加选择 onnx 模型功能

--- T1/HandwrittenCNNTestUsingOnnxUI.py
+++ T1/HandwrittenCNNTestUsingOnnxUI.py
@@ -1,7 +1,8 @@
 import sys
+import os
 import torch
 import torch.nn as nn
-from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QTextEdit, QLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QTextEdit, QFileDialog
 from PyQt6.QtGui import QPainter, QPen, QColor, QPalette, QPaintEvent,QPixmap
 from PyQt6.QtCore import Qt, QPoint
 from PIL import Image as PilImage
@@ -9,7 +10,8 @@ from torchvision import transforms
 import numpy as np
 import onnxruntime
 
-onnx_session = onnxruntime.InferenceSession("T1/mnist_cnn.onnx")
+onnx_file_path = 'T1/mnist_cnn.onnx'
+onnx_session = onnxruntime.InferenceSession(onnx_file_path)
 
 # 定义一个函数，用于读取图片并将其转换为模型可以接受的格式
 def load_and_preprocess_image(image):
@@ -106,19 +108,24 @@ class HandwrittenMainWindow(QMainWindow):
         # 创建手绘组件实例
         self.handwrittenWidget = HandwrittenWidget()
 
+        # 创建LoadModel按钮
+        self.loadButton = QPushButton('Load Model', self)
+        self.loadButton.setFixedSize(200, 40)
+        self.loadButton.clicked.connect(self.onLoadModelClicked)
+
         # 创建Clear按钮
         self.clearButton = QPushButton('Clear', self)
-        self.clearButton.setFixedSize(200, 40)
+        self.clearButton.setFixedSize(150, 40)
         self.clearButton.clicked.connect(self.handwrittenWidget.clear)
 
         # 创建Recognize按钮
         self.recognizeButton = QPushButton('Recognize', self)
-        self.recognizeButton.setFixedSize(200, 40)
+        self.recognizeButton.setFixedSize(150, 40)
         self.recognizeButton.clicked.connect(self.onRecognizeClicked)
 
         # 创建 Result 文本框
         self.result_textbox = QTextEdit(self)
-        self.result_textbox.setFixedSize(200, 40)
+        self.result_textbox.setFixedSize(100, 40)
 
         # 设置布局
         layout = QVBoxLayout()
@@ -129,6 +136,7 @@ class HandwrittenMainWindow(QMainWindow):
         topLayout.addStretch(1)
 
         bottomLayout = QHBoxLayout()
+        bottomLayout.addWidget(self.loadButton)
         bottomLayout.addWidget(self.clearButton)
         bottomLayout.addWidget(self.recognizeButton)
         bottomLayout.addWidget(self.result_textbox)
@@ -141,6 +149,26 @@ class HandwrittenMainWindow(QMainWindow):
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
+    
+    def onLoadModelClicked(self):
+        # 设置初始目录
+        current_path = os.getcwd()
+        file_dialog = QFileDialog()
+        file_dialog.setDirectory(f"{current_path}/T1/")
+        print(f"{current_path}/T1/")
+        # 设置文件过滤器，仅显示 .data 扩展名的文件
+        file_dialog.setNameFilter("Data Files (*.onnx)")
+        #file_dialog.selectNameFilter("Data Files (*.onnx)")  # 可选，确保默认选择这个过滤器
+
+        # 弹出对话框并等待用户选择
+        if file_dialog.exec():
+            # 获取选择的文件路径
+            selected_files = file_dialog.selectedFiles()
+            onnx_file_path = selected_files[0]
+            onnx_session = onnxruntime.InferenceSession(onnx_file_path)
+            self.setTitle('手写体数字识别, 当前使用模型: ' + onnx_file_path)
+        else:
+            print("No file selected.")
 
     def onRecognizeClicked(self):
         # 使用grab()方法获取子控件的内容
@@ -183,8 +211,13 @@ class HandwrittenMainWindow(QMainWindow):
         print(f'The predicted digit is: {predicted_digit}')
         self.result_textbox.setText(str(predicted_digit))
 
+    def setTitle(self, title):
+        self.setWindowTitle(title)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow  = HandwrittenMainWindow()
+    onnx_session.get_modelmeta()
+    mainWindow.setTitle(onnx_file_path)
     mainWindow.show()
     sys.exit(app.exec())

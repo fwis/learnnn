775a3b979c4d1e9b2a50385e35c17c6bfe6a9436
Asuka <454914635@qq.com>
2024-05-30 18:18:49 +0800

    实现了cam显示在UI中，加了一些保护防止图片读写等报错

---
+++ T1/NNs.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# 定义ResNet
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = nn.Sequential(*self.resnet_block(64, 64, num_blocks[0], first_block=True))
+        self.layer2 = nn.Sequential(*self.resnet_block(64, 128, num_blocks[1]))
+        self.layer3 = nn.Sequential(*self.resnet_block(128, 256, num_blocks[2]))
+        self.layer4 = nn.Sequential(*self.resnet_block(256, 512, num_blocks[3])) if num_blocks[3] > 0 else nn.Identity()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        final_out_channels = self.get_final_out_channels(block, num_blocks)
+        self.fc = nn.Linear(final_out_channels if block == self.Residual else 2048, num_classes)
+
+    # 残差块
+    class Residual(nn.Module):
+        def __init__(self, input_channels, num_channels, use_1x1conv=False, strides=1):
+            super().__init__()
+            self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+            self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+            if use_1x1conv:
+                self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+            else:
+                self.conv3 = None
+            self.bn1 = nn.BatchNorm2d(num_channels)
+            self.bn2 = nn.BatchNorm2d(num_channels)
+
+        def forward(self, X):
+            Y = F.relu(self.bn1(self.conv1(X)))
+            Y = self.bn2(self.conv2(Y))
+            if self.conv3:
+                X = self.conv3(X)
+            Y += X
+            return F.relu(Y)
+
+    def resnet_block(self, input_channels, num_channels, num_residuals, first_block=False):
+        blk = []
+        for i in range(num_residuals):
+            if i == 0 and not first_block:
+                blk.append(self.Residual(input_channels, num_channels, use_1x1conv=True, strides=2))
+            else:
+                blk.append(self.Residual(num_channels, num_channels))
+        return blk
+    
+    def get_final_out_channels(self, block, num_blocks):
+        # 计算最终的输出通道数
+        layers = [64, 128, 256, 512]
+        for i in range(4):
+            if num_blocks[i] == 0:
+                return layers[i-1] if i > 0 else 64
+        return layers[3] if block == self.Residual else layers[3] * 4
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+# FCN模型
+class FCN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(FCN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        # GAP前卷积层，输出通道数应与类别数(10个数字)相同
+        self.conv5 = nn.Conv2d(in_channels=256, out_channels=num_classes, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        #x = self.dropout(x)
+        x = F.relu(self.conv2(x))
+        #x = self.dropout(x)
+        x = F.relu(self.conv3(x))
+        #x = self.dropout(x)
+        x = F.relu(self.conv4(x))
+        #x = self.dropout(x)
+        x = self.conv5(x)  # 最后一层卷积不使用ReLU激活
+        
+        # 使用全局平均池化
+        x = nn.functional.adaptive_avg_pool2d(x, (1, 1))  # 输出形状变为(B, C, 1, 1)
+        
+        # 将四维张量展平为二维，以便进行损失计算
+        x = x.view(x.size(0), -1)
+        return x
+    
+# CNN模型
+class CNNNet(nn.Module):
+    def __init__(self):
+        super(CNNNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.fc1 = nn.Linear(7*7*64, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 7*7*64)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
---
+++ T1/UICopy.py
@@ -0,0 +1,373 @@
+import sys
+import os
+import torch
+import torch.nn as nn
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QTextEdit, QFileDialog, QLabel, QMessageBox, QCheckBox
+from PyQt6.QtGui import QPainter, QPen, QColor, QPalette, QPaintEvent, QPixmap, QImage
+from PyQt6.QtCore import Qt, QPoint
+from PIL import Image as PilImage
+from torchvision import transforms
+import numpy as np
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
+import cv2
+import NNs as mynns
+
+pth_file_path = 'D:/Neural_Networks/learnnn/T2/mnist_ResNet2.pth'
+num_blocks = [1, 1, 0, 0]
+num_classes = 10
+pth_model = mynns.ResNet(mynns.ResNet.Residual, num_blocks, num_classes=num_classes)
+pth_model.load_state_dict(torch.load(pth_file_path, map_location=torch.device('cpu')))
+pth_model.eval()  # 设置模型为评估模式
+
+# 定义一个函数，用于读取图片并将其转换为模型可以接受的格式
+def load_and_preprocess_image(image):
+    transform = transforms.Compose([
+        transforms.Resize((28, 28)),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    image = transform(image).unsqueeze(0)  # 增加一个维度，因为模型期望的输入是[batch_size, channels, height, width]
+    return image
+
+
+class HandwrittenWidget(QWidget):
+    def __init__(self, parent=None):
+        super(HandwrittenWidget, self).__init__(parent)
+        self.setFixedSize(280, 280)  # 设置固定大小
+        palette = self.palette()  # 获取当前窗口的调色板
+
+        palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.black)  # 设置Window角色的颜色为黑色
+
+        self.setPalette(palette)  # 应用新的调色板到窗口
+        self.setAutoFillBackground(True)
+        
+        self.drawing = False
+        self.points_group = []
+        self.points = []
+        self.points_group.append(self.points)
+        self.image = None  # 存储加载的图像
+
+    def paintEvent(self, event: QPaintEvent):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.GlobalColor.white, 13, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
+        if self.image:
+            painter.drawPixmap(QPoint(0, 0), self.image) 
+        if self.drawing:
+            for points in self.points_group:
+                p1 = None
+                for p2 in points:
+                    if p1 != None:
+                        painter.drawLine(p1, p2)
+                    p1 = p2
+        else:
+            #print('nnnn')
+            pass
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.points = []
+            self.points_group.append(self.points)
+            self.drawing = True
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            #self.drawing = False
+            pass
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
+            self.points.append(event.pos())
+            self.update()
+
+    def clear(self):
+        self.points_group.clear()
+        self.points.clear()
+        self.image = None
+        self.update()
+
+    def getPixmap(self):
+        # 创建一个QPixmap，其大小和Widget相同
+        pixmap = QPixmap(self.size())
+        # 用QPainter将Widget的内容绘制到pixmap上
+        painter = QPainter(pixmap)
+        painter.drawPixmap(QPoint(0, 0), pixmap)
+        self.render(painter)
+        return pixmap
+    
+    def save_image(self, file_path):
+        pixmap = self.getPixmap()
+        pixmap.save(file_path)
+        
+    def set_image(self, image):
+        self.image = image
+        self.update()
+    
+class CamWidget(QWidget):
+    def __init__(self, parent=None):
+        super(CamWidget, self).__init__(parent)
+        self.setFixedSize(280, 280)  # 设置固定大小
+        self.label = QLabel(self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+        
+        palette = self.palette()  # 获取当前窗口的调色板
+        palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.black)  # 设置Window角色的颜色为黑色
+        self.setPalette(palette)  # 应用新的调色板到窗口
+        self.setAutoFillBackground(True)
+
+    def set_heatmap(self, heatmap):
+        print('set_heatmap')
+        black_image = np.zeros((280, 280, 3), dtype=np.uint8)  # 三通道 RGB 图像
+        black_height, black_width, black_channels = black_image.shape
+        black_qimage = QImage(black_image.data, black_width, black_height, 3 * black_width, QImage.Format.Format_RGB888)
+        black_pixmap = QPixmap.fromImage(black_qimage)
+        self.label.setPixmap(black_pixmap)
+        # 将 numpy 数组转换为 QImage
+        heatmap = cv2.resize(heatmap, (280, 280), interpolation=cv2.INTER_LINEAR)
+        height, width, channels = heatmap.shape
+        qimage = QImage(heatmap.data, width, height, 3 * width, QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimage)
+        self.label.setPixmap(pixmap)
+        self.label.repaint()
+    
+    def clear(self):
+        black_image = np.zeros((280, 280, 3), dtype=np.uint8)  # 三通道 RGB 图像
+        height, width, channels = black_image.shape
+        qimage = QImage(black_image.data, width, height, 3 * width, QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimage)
+        self.label.setPixmap(pixmap)
+
+def predict_digit(pil_image):
+    tensor_image = load_and_preprocess_image(pil_image) # (1, 1, 28, 28)
+    with torch.no_grad():
+        output = pth_model(tensor_image)
+        target_layers = [pth_model.layer2[-1]]
+        predicted_digit = output.argmax(dim=1).item()
+
+    heatmap = generate_cam_heatmap(target_layers, tensor_image)
+
+    return predicted_digit, heatmap
+
+def generate_cam_heatmap(target_layer, tensor_image):
+    
+    # 生成Grad-CAM heatmap（对一个图像）
+    cam = GradCAM(model=pth_model, target_layers=target_layer)
+    mask = cam(input_tensor=tensor_image)[0, :] # (H, W, C)
+    
+    # 转换成图像
+    gray_img = tensor_image.squeeze().numpy()
+    rgb_img_hwc = np.repeat(gray_img[:, :, np.newaxis], 3, axis=2) # (H, W, C)
+    rgb_img_hwc = cv2.normalize(rgb_img_hwc, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+    visualization_c3u8_rgb = show_cam_on_image(rgb_img_hwc, mask, use_rgb=True)
+    
+    return visualization_c3u8_rgb
+
+class HandwrittenMainWindow(QMainWindow):
+    def __init__(self):
+        super(HandwrittenMainWindow, self).__init__()
+        self.setWindowTitle('手写数字识别程序')
+        self.setGeometry(100, 100, 800, 600)  # 增加宽度以适应热力图窗口
+        self.handwrittenWidget = HandwrittenWidget()
+        self.camWidget = CamWidget()  # 创建热力图组件
+        self.processed_image = None
+
+        # 创建LoadModel按钮
+        self.loadModelButton = QPushButton('Load Model', self)
+        self.loadModelButton.setFixedSize(150, 40)
+        self.loadModelButton.clicked.connect(self.onLoadModelClicked)
+
+        # 创建LoadImage按钮
+        self.loadImageButton = QPushButton('Load Image', self)
+        self.loadImageButton.setFixedSize(150, 40)
+        self.loadImageButton.clicked.connect(self.onLoadImageClicked)
+        
+        # 创建Clear按钮
+        self.clearButton = QPushButton('Clear', self)
+        self.clearButton.setFixedSize(150, 40)
+        self.clearButton.clicked.connect(self.handwrittenWidget.clear)
+        self.clearButton.clicked.connect(self.clearProcessedImage)
+        self.clearButton.clicked.connect(self.camWidget.clear)
+
+        # 创建Recognize按钮
+        self.recognizeButton = QPushButton('Recognize', self)
+        self.recognizeButton.setFixedSize(150, 40)
+        self.recognizeButton.clicked.connect(self.onRecognizeClicked)
+
+        # 创建 Save 按钮
+        self.saveButton = QPushButton('Save Image', self)
+        self.saveButton.setFixedSize(150, 40)
+        self.saveButton.clicked.connect(self.onSaveImageClicked)
+        
+        # 创建 Save Processed Image 按钮
+        self.saveProcessedButton = QPushButton('Save Processed Image', self)
+        self.saveProcessedButton.setFixedSize(150, 40)
+        self.saveProcessedButton.clicked.connect(self.onSaveProcessedImageClicked)
+        
+        # 创建 Result 文本框
+        self.result_textbox = QTextEdit(self)
+        self.result_textbox.setFixedSize(100, 40)
+
+        # 设置布局
+        layout = QVBoxLayout()
+        
+        topLayout = QHBoxLayout()
+        topLayout.addStretch(1)
+        topLayout.addWidget(self.handwrittenWidget)
+        topLayout.addWidget(self.camWidget)  # 添加热力图组件
+        topLayout.addStretch(1)
+
+        bottomLayout = QHBoxLayout()
+        bottomLayout.addWidget(self.loadModelButton)
+        bottomLayout.addWidget(self.loadImageButton)
+        bottomLayout.addWidget(self.clearButton)
+        bottomLayout.addWidget(self.recognizeButton)
+        bottomLayout.addWidget(self.saveButton)
+        bottomLayout.addWidget(self.saveProcessedButton)
+        bottomLayout.addWidget(self.result_textbox)
+        #  bottomLayout.setSizeConstraint(QLayout.SetFixedSize)
+
+        layout.addLayout(topLayout)
+        layout.addLayout(bottomLayout)
+
+        # 设置中心窗口
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+        
+    def clearProcessedImage(self):
+        self.processed_image = None
+    
+    def onLoadModelClicked(self):
+        global pth_model
+        # 设置初始目录
+        current_path = os.getcwd()
+        file_dialog = QFileDialog()
+        file_dialog.setDirectory(f"{current_path}/T1/")
+        print(f"{current_path}/T1/")
+        # 设置文件过滤器，仅显示 .data 扩展名的文件
+        file_dialog.setNameFilter("Data Files (*.pth)")
+        #file_dialog.selectNameFilter("Data Files (*.onnx)")  # 可选，确保默认选择这个过滤器
+
+        # 弹出对话框并等待用户选择
+        if file_dialog.exec():
+            # 获取选择的文件路径
+            selected_files = file_dialog.selectedFiles()
+            pth_file_path = selected_files[0]
+            if 'ResNet2' in pth_file_path:
+                num_blocks = [1, 1, 0, 0]
+                pth_model = mynns.ResNet(mynns.ResNet.Residual, num_blocks, num_classes)
+                pth_model.load_state_dict(torch.load(pth_file_path, map_location=torch.device('cpu')))
+            elif 'fcn' in pth_file_path:
+                pth_model = mynns.FCN(num_classes)
+                pth_model.load_state_dict(torch.load(pth_file_path, map_location=torch.device('cpu')))
+            elif 'cnn' in pth_file_path:
+                pth_model = mynns.CNNNet()
+                pth_model.load_state_dict(torch.load(pth_file_path, map_location=torch.device('cpu')))
+            self.setTitle('手写体数字识别, 当前使用模型: ' + pth_file_path)
+        else:
+            print("No file selected.")
+
+    def onLoadImageClicked(self):
+        # 打开文件对话框以选择图像文件
+        file_path, _ = QFileDialog.getOpenFileName(self, "打开图像", "", "图像文件 (*.png *.jpg *.bmp)")
+        if file_path:
+            pil_image = PilImage.open(file_path).convert('L')  # 转换为灰度图像
+            pil_image_resized = pil_image.resize((280, 280))  # 调整大小
+        
+            # 显示选中的图像
+            self.handwrittenWidget.clear()
+            self.handwrittenWidget.repaint()  # 清除之前的内容
+            qimage = QImage(pil_image_resized.tobytes(), pil_image_resized.width, pil_image_resized.height, QImage.Format.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(qimage)
+            # self.handwrittenWidget.update()
+            self.handwrittenWidget.set_image(pixmap)
+            
+        else:
+            print("未选择文件。")
+
+    def onRecognizeClicked(self):
+        # 使用grab()方法获取子控件的内容
+        qpixmap = self.handwrittenWidget.getPixmap()
+        #qpixmap.save('d.bmp');
+        # 将QPixmap转换为QImage
+        qimage = qpixmap.toImage()
+
+        # 将QImage转换为PIL的Image
+        pil_image = PilImage.fromqimage(qimage)
+
+        np_image = np.array(pil_image)
+
+        # 找到白色区域的索引
+        white_pixels = np.where(np_image > 200)
+
+        if len(white_pixels[0]) == 0 or len(white_pixels[1]) == 0:
+            QMessageBox.warning(self, "Warning", "Please draw a figure or load an image first.")
+            return
+        
+        # 计算白色区域的最小外包矩形
+        min_x, max_x = np.min(white_pixels[1]), np.max(white_pixels[1])
+        min_y, max_y = np.min(white_pixels[0]), np.max(white_pixels[0])
+
+        # 计算扩展后的正方形尺寸
+        size = max(max_x - min_x + 1, max_y - min_y + 1)
+        size = int(size * 1.2)
+
+        # 计算粘贴位置，使原始内容位于正方形中央
+        paste_pos = ((size - (max_x - min_x + 1)) // 2, (size - (max_y - min_y + 1)) // 2)
+
+        sub_np_img = np_image[min_y:max_y,min_x:max_x]
+        # 将原始白色区域裁剪并粘贴到正方形中心
+        sub_pil_img = PilImage.fromarray(sub_np_img)
+        #sub_pil_img.show()
+
+        # 创建一个新的正方形图像，初始填充为黑色
+        result_pil_img = PilImage.new('L', (size, size), 0)
+        result_pil_img.paste(sub_pil_img, paste_pos)
+        self.processed_image = result_pil_img
+        #result_pil_img.show()
+
+        # 使用predict_digit函数进行预测
+        predicted_digit, heatmap = predict_digit(result_pil_img)
+        print(f'The predicted digit is: {predicted_digit}')
+        self.result_textbox.setText(str(predicted_digit))
+        self.camWidget.set_heatmap(heatmap)  # 显示热力图
+
+    def onSaveProcessedImageClicked(self):
+        if self.processed_image:
+            file_path, _ = QFileDialog.getSaveFileName(self, "Save Processed Image", "", "PNG Files (*.png);;JPEG Files (*.jpg)")
+            if file_path:
+                processed_image_resized = self.processed_image.resize((28, 28))
+                processed_image_resized.save(file_path)
+            else:
+                print("No file selected.")
+        else:
+            QMessageBox.warning(self, "No Processed Image", "There is no processed image to save. Please recognize an image first.")
+    
+    def onSaveImageClicked(self):
+        qimage = self.handwrittenWidget.getPixmap()
+        pil_image = PilImage.fromqimage(qimage)
+        np_image = np.array(pil_image)
+        
+        white_pixels = np.where(np_image > 200)
+        if len(white_pixels[0]) == 0 or len(white_pixels[1]) == 0:
+            QMessageBox.warning(self, "Warning", "Please draw figure first.")
+        else:
+            file_path, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "PNG Files (*.png);;JPEG Files (*.jpg)")
+            if file_path:
+                self.handwrittenWidget.save_image(file_path)
+    
+    def setTitle(self, title):
+        self.setWindowTitle(title)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = HandwrittenMainWindow()
+    mainWindow.setTitle('手写数字识别程序')
+    mainWindow.show()
+    sys.exit(app.exec())

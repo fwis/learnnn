1d09e91e76379c09d5b46d7d95722b5477c1eaff
LiuHr <1018421493@qq.com>
2024-06-05 09:11:31 +0800

    训练了一版ResNet

--- T3/Frame/model.py
+++ T3/Frame/model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
 import math
 import h5py
 
@@ -42,8 +43,79 @@ class ForkNet(nn.Module):
         aop = torch.atan(aop) / 2. + math.pi / 4
         
         return s0, dolp, aop
-
     
+    
+# Residual block
+class Residual(nn.Module):
+    def __init__(self, input_channels, num_channels, use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
+
+def resnet_block(input_channels, num_channels, num_residuals, first_block=False):
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.append(Residual(input_channels, num_channels, use_1x1conv=True, strides=1))
+        else:
+            blk.append(Residual(num_channels, num_channels))
+    return blk
+
+class ResNet(nn.Module):
+    def __init__(self, num_blocks=[2, 2, 2, 2]):
+        super(ResNet, self).__init__()
+        # self.in_channels = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, stride=1, padding=4, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.layer1 = nn.Sequential(*resnet_block(32, 64, num_blocks[0], first_block=True))
+        self.layer2 = nn.Sequential(*resnet_block(64, 128, num_blocks[1]))
+        self.layer3 = nn.Sequential(*resnet_block(128, 64, num_blocks[2]))
+        
+        self.layer4_1 = nn.Sequential(*resnet_block(64, 32, num_blocks[3])) if num_blocks[3] > 0 else nn.Identity()
+        self.layer4_2 = nn.Sequential(*resnet_block(64, 32, num_blocks[3])) if num_blocks[3] > 0 else nn.Identity()
+        self.layer4_3 = nn.Sequential(*resnet_block(64, 32, num_blocks[3])) if num_blocks[3] > 0 else nn.Identity()
+        
+        self.conv_1 = nn.Conv2d(32,1,kernel_size=3,stride=1,padding=1,)
+        self.conv_2 = nn.Conv2d(32,1,kernel_size=3,stride=1,padding=1,)
+        self.conv_3 = nn.Conv2d(32,1,kernel_size=3,stride=1,padding=1,)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+                
+        aop = self.layer4_1(x)
+        aop = self.conv_1(aop)
+        aop = torch.atan(aop) / 2. + math.pi / 4
+        
+        dolp = self.layer4_2(x)
+        dolp = self.conv_2(dolp)
+        
+        s0 = self.layer4_3(x)
+        s0 = self.conv_3(s0)
+        
+        return aop, dolp, s0
+
+
 class MyDataset(Dataset):
     def __init__(self, file_path) -> None:
         super(MyDataset, self).__init__()
@@ -61,22 +133,8 @@ class MyDataset(Dataset):
             dolp = torch.from_numpy(h5file[f'labels/label_{idx}/dolp'][...]).unsqueeze(0)
             s0 = torch.from_numpy(h5file[f'labels/label_{idx}/s0'][...]).unsqueeze(0)
         return data, aop, dolp, s0
-    
-    
-# file_path = r"D:\WORKS\Polarization\data\data_h5\data_ol.h5"
-# batch_size = 10
-
-# custom_dataset = MyDataset(file_path)
-# data_loader = torch.utils.data.DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
-
-
-# for i, (data,aop,dolp,s0) in enumerate(data_loader):
-#     print("Batch", i+1)
-#     print("Data tensor shape:", data.shape)
-#     print("Labels tensor shape:", aop.shape)
 
 
 '''
 Loss Functions
 '''
-
---
+++ T3/Frame/test.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset, random_split
+from torch.optim.lr_scheduler import ExponentialLR
+import numpy as np
+import matplotlib.pyplot as plt
+from model import MyDataset, ForkNet, ResNet
+import warnings
+
+# Suppress specific warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="Unsupported Windows version")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = ResNet()  
+
+# Load checkpoint
+checkpoint_path = r'D:\VScodeProjects\learnnn\T3\Frame\ckpt\ResNet.pth'
+checkpoint = torch.load(checkpoint_path)
+model.load_state_dict(checkpoint['model_state_dict'])
+model = model.to(device)
+
+model.eval()
+
+input_data_path = r'D:\VScodeProjects\learnnn\T3\Frame\test\13_data.pt'
+img = torch.load(input_data_path).unsqueeze(0).unsqueeze(0).to(device)
+
+# forward
+with torch.no_grad():
+    aop, dolp, s0 = model(img)
+
+aop = aop.cpu().squeeze().numpy()
+dolp = dolp.cpu().squeeze().numpy()
+s0 = s0.cpu().squeeze().numpy()
+
+# Plot results
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+
+axs[0].imshow(aop, cmap='gray')
+axs[0].set_title('AOP')
+
+axs[1].imshow(dolp, cmap='gray')
+axs[1].set_title('DoLP')
+axs[2].imshow(s0, cmap='gray')
+axs[2].set_title('S0')
+
+plt.tight_layout()
+plt.show()

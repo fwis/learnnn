cbcd26cd274854c76a4b79ab6aa39510b3926646
smithfox <ssm.fox@gmail.com>
2024-06-20 12:34:37 +0800

    训练最高128通道的网络，效果还不错

--- T3/Frame/oonx.py
+++ T3/Frame/oonx.py
@@ -2,11 +2,11 @@ import torch
 from model import ForkNet, ResNet
  
 model = ResNet()
-checkpoint = torch.load(r'T3\Frame\ckpt\ResNet2.pth')
+checkpoint = torch.load(r'T3\Frame\ckpt\ResNet4.pth')
 model.eval()
 dummy_input = torch.randn(1, 1, 100, 100)
 
-torch.onnx.export(model, dummy_input, r'T3\Frame\ckpt\ResNet2.onnx',verbose=True)
+torch.onnx.export(model, dummy_input, r'T3\Frame\ckpt\ResNet4.onnx',verbose=True)
 
 # 多输入,opset_version根据torch的版本决定，转onnx的时候提示的
 # torch.onnx.export(model, [dummy_input,dummy_input_2], 'name.onnx',verbose=True,opset_version=9/10/11)

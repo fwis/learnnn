4595f7e0f9c562e8c82bfd9567fc8012a8cdcc4c
smithfox <ssm.fox@gmail.com>
2024-06-26 10:55:51 +0800

    Revert "Merge branch 'main' of https://github.com/fwis/learnnn"
    This reverts commit a6f0b993687146123fafcd9d5b90511aa7494824, reversing
    changes made to 9e3aab2e03db190316cbbbae3fff6e8339ab70e9.

--- T3/Frame/CAM.py
+++ T3/Frame/CAM.py
@@ -5,13 +5,12 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 import numpy as np
 import cv2
-from model import ConvNeXtNet, ResNet
+from model import ConvNeXtNet
 import matplotlib.pyplot as plt
 
-# Multi-task to Single-task
-class single_task_model_wrapper(nn.Module):
+class SingleTaskModelWrapper(nn.Module):
     def __init__(self, model, task_index):
-        super(single_task_model_wrapper, self).__init__()
+        super(SingleTaskModelWrapper, self).__init__()
         self.model = model
         self.task_index = task_index
 
@@ -19,12 +18,12 @@ class single_task_model_wrapper(nn.Module):
         outputs = self.model(x)
         return outputs[self.task_index]
 
-class image_target:
+class ImageReconstructionTarget:
     def __init__(self, original_image):
         self.original_image = original_image
 
     def __call__(self, model_output):
-        # Calculate MSE
+        # 计算输出与原始图像之间的差异
         loss = ((model_output - self.original_image) ** 2).mean()
         return loss
 
@@ -36,22 +35,23 @@ checkpoint = torch.load(checkpoint_path)
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 model.to(device)
-task_index = 1 # [0, 1, 2] = aop, dolp, s0
-single_task_model = single_task_model_wrapper(model, task_index)
+task_index = 0
+single_task_model = SingleTaskModelWrapper(model, task_index)
 
 # Set target layer
-target_layer = single_task_model.model.layer3_2
+target_layer = single_task_model.model.conv2_1
 
 # Create GradCAM instance
 cam = GradCAM(model=single_task_model, target_layers=[target_layer])
 
 # Input image
-input_tensor = torch.load(r'T3\Frame\test\7_data.pt').to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-input_tensor = input_tensor[200:1000,200:1000].unsqueeze(0).unsqueeze(0)
+input_tensor = torch.load(r'T3\Frame\test\83_data.pt').to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+input_tensor = input_tensor[:500,:500].unsqueeze(0).unsqueeze(0)
+# Original image for calculating reconstruction error
 original_image = input_tensor.clone()
 
 # Generate CAM heatmap
-targets = [image_target(original_image)]
+targets = [ImageReconstructionTarget(original_image)]
 grayscale_cam = cam(input_tensor=input_tensor, targets=targets)[0]
 grayscale_cam = cv2.normalize(grayscale_cam, None, 0, 1, cv2.NORM_MINMAX)
 
@@ -62,9 +62,8 @@ bgr_image = cv2.normalize(bgr_image, None, 0, 1, cv2.NORM_MINMAX).astype(np.uint
 
 # Generate visualization image
 visualization = show_cam_on_image(bgr_image, grayscale_cam, image_weight=0.6)
-torch.cuda.empty_cache()
 
-# Show image
+# 显示结果
 cv2.namedWindow('Grad-CAM', cv2.WINDOW_NORMAL)
 cv2.imshow('Grad-CAM', visualization)
 cv2.waitKey(0)
